658f3e5e8f7cd874b0d8e208eae30a5c3c60c8bb
clement-ux <clemmoller@gmail.com>
2024-05-13 09:13:10 +0200

    feat: :sparkles: Add Locker modifiers for locking and freezing.

--- test/unit/shared/Modifiers.sol
+++ test/unit/shared/Modifiers.sol
@@ -4,6 +4,32 @@ pragma solidity 0.8.23;
 import {Base_Test_} from "../../Base.sol";
 
 contract Modifiers is Base_Test_ {
+    /*//////////////////////////////////////////////////////////////
+                                STRUCTS
+    //////////////////////////////////////////////////////////////*/
+
+    // --- Locker ---
+
+    struct Modifier_Lock {
+        uint256 skipBefore;
+        address user;
+        uint256 amountToLock;
+        uint256 duration;
+        uint256 skipAfter;
+    }
+
+    struct Modifier_Freeze {
+        uint256 skipBefore;
+        address user;
+        uint256 skipAfter;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                               MODIFIERS
+    //////////////////////////////////////////////////////////////*/
+
+    // --- Core Owner ---
+
     modifier asOwner() {
         vm.startPrank(coreOwner.owner());
         _;
@@ -14,5 +40,38 @@ contract Modifiers is Base_Test_ {
         vm.prank(coreOwner.owner());
         coreOwner.commitTransferOwnership(newOwner);
         _;
+    }
+
+    // --- Locker ---
+
+    modifier lock(Modifier_Lock memory _lock) {
+        _modifierLock(_lock);
+        _;
+    }
+
+    modifier freeze(Modifier_Freeze memory _freeze) {
+        _modifierFreeze(_freeze);
+        _;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                               FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    // --- Locker ---
+
+    function _modifierLock(Modifier_Lock memory _lock) internal {
+        skip(_lock.skipBefore);
+        deal(address(govToken), _lock.user, _lock.amountToLock * 1 ether);
+        vm.prank(_lock.user);
+        tokenLocker.lock(_lock.user, _lock.amountToLock, _lock.duration);
+        skip(_lock.skipAfter);
+    }
+
+    function _modifierFreeze(Modifier_Freeze memory _freeze) internal {
+        skip(_freeze.skipBefore);
+        vm.prank(_freeze.user);
+        tokenLocker.freeze();
+        skip(_freeze.skipAfter);
     }
 }
