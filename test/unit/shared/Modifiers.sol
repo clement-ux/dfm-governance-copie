3c9eb0d7228765764b545ec2d931863a1acc590f
clement-ux <clemmoller@gmail.com>
2024-05-13 10:51:11 +0200

    feat: :sparkles: Add LockMany to Modifiers.

--- test/unit/shared/Modifiers.sol
+++ test/unit/shared/Modifiers.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.23;
 
 import {Base_Test_} from "../../Base.sol";
+import {TokenLockerBase} from "../../../contracts/dependencies/TokenLockerBase.sol";
 
 contract Modifiers is Base_Test_ {
     /*//////////////////////////////////////////////////////////////
@@ -15,6 +16,14 @@ contract Modifiers is Base_Test_ {
         address user;
         uint256 amountToLock;
         uint256 duration;
+        uint256 skipAfter;
+    }
+
+    struct Modifier_LockMany {
+        uint256 skipBefore;
+        address user;
+        uint64[5] amountToLock;
+        uint8[5] duration;
         uint256 skipAfter;
     }
 
@@ -49,6 +58,11 @@ contract Modifiers is Base_Test_ {
         _;
     }
 
+    modifier lockMany(Modifier_LockMany memory _lockMany) {
+        _modifierLockMany(_lockMany);
+        _;
+    }
+
     modifier freeze(Modifier_Freeze memory _freeze) {
         _modifierFreeze(_freeze);
         _;
@@ -66,6 +80,33 @@ contract Modifiers is Base_Test_ {
         vm.prank(_lock.user);
         tokenLocker.lock(_lock.user, _lock.amountToLock, _lock.duration);
         skip(_lock.skipAfter);
+    }
+
+    function _modifierLockMany(Modifier_LockMany memory _lockMany) internal {
+        skip(_lockMany.skipBefore);
+
+        // Find correct lenght for the array
+        uint256 len;
+        for (uint256 i; i < _lockMany.amountToLock.length; i++) {
+            if (_lockMany.amountToLock[i] == 0) {
+                len = i;
+                break;
+            }
+        }
+
+        TokenLockerBase.LockData[] memory data = new TokenLockerBase.LockData[](len);
+        uint256 totalAmount;
+        for (uint256 i = 0; i < len; i++) {
+            uint256 amount = _lockMany.amountToLock[i];
+
+            totalAmount += amount;
+            data[i] = TokenLockerBase.LockData({amount: amount, epochsToUnlock: _lockMany.duration[i]});
+        }
+
+        deal(address(govToken), _lockMany.user, totalAmount * 1 ether);
+        vm.prank(_lockMany.user);
+        tokenLocker.lockMany(_lockMany.user, data);
+        skip(_lockMany.skipAfter);
     }
 
     function _modifierFreeze(Modifier_Freeze memory _freeze) internal {
