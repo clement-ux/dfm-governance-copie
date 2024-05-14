19b25b01344a1bf95b7a88b30f283571c8611d0b
clement-ux <clemmoller@gmail.com>
2024-05-14 13:45:02 +0200

    fix: :fire: Remove unnecessary imports.

--- test/unit/concrete/token-locker/Constructor.t.sol
+++ test/unit/concrete/token-locker/Constructor.t.sol
@@ -1,25 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-import {Vm} from "forge-std/Vm.sol";
-import {stdError} from "forge-std/StdError.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import {TokenLocker} from "../../../../contracts/TokenLocker.sol";
 import {TokenLockerBase} from "../../../../contracts/dependencies/TokenLockerBase.sol";
 import {IIncentiveVoting} from "../../../../contracts/interfaces/IIncentiveVoting.sol";
 import {Unit_Shared_Test_} from "../../shared/Shared.sol";
-import {WizardTokenLocker} from "../../../utils/WizardTokenLocker.sol";
 
 contract Unit_Concrete_TokenLocker_Constructor_ is Unit_Shared_Test_ {
-    using WizardTokenLocker for Vm;
-
     uint256 internal startTime;
 
     /*//////////////////////////////////////////////////////////////
                                  SETUP
     //////////////////////////////////////////////////////////////*/
 
+    /// @notice Deploy the contracts and check the initial state
     function test_TokenLocker_Constructor() public {
         tokenLocker = new TokenLocker(
             address(coreOwner),
