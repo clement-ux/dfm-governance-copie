611459b3e2ac2b25652149a6fa98c5b57d3e7d93
clement-ux <clemmoller@gmail.com>
2024-05-18 17:39:02 +0200

    feat: :tada: Implement library for default values during tests.

---
+++ test/utils/DefaultValues.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+library DefaultValues {
+    uint256 public constant LOCK_AMOUNT = 1 ether;
+    uint256 public constant LOCK_DURATION_W = 5; // in weeks
+}
